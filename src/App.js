3a444b661773ce2f838126c94a92ba52f1ef0376
Reginaldo Boeke <reginaldoboeke@gmail.com>
2020-04-20 10:58:38 -0300

    Funcionalidades de listar, adicionar e remover.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  async function loadRepositories() {
+    try {
+      const { data } = await api.get('/repositories');
+
+      setRepositories(data);
+
+    } catch (ex) {
+      alert('Ops! Problemas ao buscar repositórios.');
+    }
+  }
+
   async function handleAddRepository() {
-    // TODO
+    try {
+      const repository = {
+        title: 'Conceitos do ReactJS',
+        url: 'https://github.com/reginaldoboeke/conceitos-reactjs',
+        techs: ['JavaScript', 'ReactJS'],
+      };
+
+      const { data } = await api.post('repositories', repository);
+
+      setRepositories([...repositories, data]);
+
+    } catch (ex) {
+      alert('Ops! Problemas ao tentar adicionar repositório.');
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      await api.delete(`/repositories/${id}`);
+
+      setRepositories(repositories.filter(repository => repository.id !== id));
+
+    } catch (ex) {
+      alert('Ops! Problemas ao tentar remover repositório.');
+    }
   }
+
+  useEffect(() => { loadRepositories() }, []);
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
